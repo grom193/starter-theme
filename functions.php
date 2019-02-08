a9ac1fada8473630aa2fafdb5d093ba060dc1d0d
Igor Sivolovskiy <igor.sivolovskiy@gmail.com>
2019-02-08 09:44:47 +0200

    added menu call with menu args

--- functions.php
+++ functions.php
@@ -63,7 +63,8 @@ class StarterSite extends Timber\Site {
 		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new Timber\Menu();
+        $args = ['menu_class' => ''];
+        $context['main_menu'] = new \Timber\Menu( 'main_menu', $args );
 		$context['site'] = $this;
 		return $context;
 	}
